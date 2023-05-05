2da2422a002594d5c9debc986130c3d898ece8ed
Riley Madden <rileymadden7@gmail.com>
2023-05-04 17:02:01 -0700

    swap cases

---
+++ Python/Easy/swap_cases.py
@@ -0,0 +1,16 @@
+def swap_case(s):
+    new_case = []
+    for char in s:
+        if char.isupper() == True:
+            new_case.append(char.lower())
+        elif char.islower() == True:
+            new_case.append((char.upper()))
+        else:
+            new_case.append(char)
+    result = ''.join(new_case) 
+    return result
+
+if __name__ == '__main__':
+    s = input("Enter a string to change the case of: ")
+    result = swap_case(s)
+    print(result)
